67bf5f08e2a827a018769605bcbd77b7e33861a6
Chris Chu <c20711@gmail.com>
2017-03-25 13:54:25 +1100

    3/25 update

--- src/M1/Driver.java
+++ src/M1/Driver.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class Driver {
 	static Scanner sc = new Scanner(System.in);
-
+	boolean end = false;
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 
 		Driver driver = new Driver();
-		driver.menu();
 		driver.selection();
+		driver.runGame();
 	}
-
-	public void menu() {
+// Create a menu list 
+	public void menu() {   
 		System.out.println("Ozlympic Game");
 		System.out.println("===================================");
 		System.out.println("1. Select a game to run");
@@ -24,24 +29,51 @@ public class Driver {
 		System.out.print("Enter an option: ");
 
 	}
+	
+	public void menu_selection1(){
+		System.out.println("Please choose a game");
+		System.out.println("1. Swimming");
+		System.out.println("2. Cycling");
+		System.out.println("3. Running");
+		System.out.println("4. Previous menu");
+	}
 
+	
+//	public void menu_selection2(){
+//		for (int i<0 ; i<x.length ;i++){
+//			
+//		}
+//	}
+	
+	
+	
+	
 	public int selection() {
 		int selection = 0;
 		boolean validInput = false;
 		do {
+			menu();
 			validInput = true;
 			try {
 				selection = sc.nextInt();
 			} catch (Exception e) {
 				validInput = false;
-				// sc.next();
+				System.out.print("\n========Your input: " + sc.next() + " is invalid========\n\n");
+				sc.next();
 			}
 			if (selection < 0 || selection > 6) {
-				System.out.print("\n========Your input: " + sc.next() + " is invalid========\n\n");
-				menu();
+				System.out.print("\n========Your input: " + selection + " is invalid========\n\n");
 			}
 
 		} while (!validInput || selection < 0 || selection > 6);
 		return selection;
 	}
+
+	public void runGame() {
+		while(!end){
+			
+		}
+		
+	}
+
 }
